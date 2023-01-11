2bd680d66a9f1eefc9d755f40f1a1a1cf4472dc5
Bryan Macias <bm577703@gmail.com>
2023-01-11 18:47:09 -0500

    Agregar img al drive

---
+++ src/node/drive/driveControllers.ts
@@ -0,0 +1,76 @@
+import { google } from 'googleapis';
+import fs from 'fs';
+import path from 'path';
+
+const oauth2Client = new google.auth.GoogleAuth({
+    credentials: {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        type: 'service_account'
+    },
+    scopes: [
+        'https://www.googleapis.com/auth/drive'
+    ]
+}
+);
+
+const drive = google.drive({
+    version: 'v3',
+    auth: oauth2Client
+});
+
+const uploadFile = async (file: any) => {
+    const filepath = path.join('temp/' + file.filename);
+    try {
+        const res = await drive.files.create({
+            requestBody: {
+                name: file.filename,
+                mimeType: file.type,
+                parents: [`${process.env.GOOGLE_CLIENT_FOLDERIMG}`],
+            },
+            media: {
+                mimeType: file.type,
+                body: fs.createReadStream(filepath)
+            }
+        });
+        fs.unlinkSync(filepath)
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteFile = async (fileId: string) => {
+    try {
+        await drive.files.delete({ fileId });
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+const generatePublicUrl = async (fileId: string) => {
+    try {
+        await drive.permissions.create({
+            fileId,
+            requestBody: {
+                role: 'reader',
+                type: 'anyone'
+            }
+        })
+        const res = await drive.files.get({ fileId, fields: 'webViewLink' });
+        const { webViewLink } = res.data as { webViewLink: string };
+        const arr = webViewLink.split('/');
+        return `https://drive.google.com/uc?export=view&id=${arr[arr.length - 2]}`
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { uploadFile, deleteFile, generatePublicUrl };
+
+
+
+
---
+++ src/pages/api/img/index.ts
@@ -0,0 +1,80 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { uploadFile, deleteFile, generatePublicUrl } from '@/node/drive/driveControllers';
+import nextConnect from 'next-connect';
+import multer from 'multer';
+import pool from "@/node/config/db";
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'temp');
+        },
+        filename: (req, file, cb) => {
+            console.log("FILENAME", file)
+            cb(null, file.originalname);
+        },
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // no larger than 5mb
+    },
+});
+
+
+const apiRout = nextConnect({
+    onNoMatch(req: NextApiRequest, res: NextApiResponse) {
+        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+    },
+    onError(error: any, req: NextApiRequest, res: NextApiResponse) {
+        res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+    },
+});
+
+apiRout.use(upload.single('file'));
+
+apiRout.post(async (req: any, res: NextApiResponse) => {
+    const { file } = req;
+    console.log(file)
+    const uploadedFile = await uploadFile(file) as { id: string, name: string };
+    if (!uploadedFile) {
+        return res.status(500).json({
+            message: 'Error uploading file'
+        });
+    }
+    const publicUrl = await generatePublicUrl(uploadedFile.id);
+    if (!publicUrl) {
+        return res.status(500).json({
+            message: 'Error generating public url'
+        });
+    }
+    await pool.query(
+        "INSERT INTO images SET ?",
+        { name: file.originalname, url: publicUrl, id_image: uploadedFile.id, externalId: uploadedFile.id }
+    );
+    return res.status(200).json({
+        message: 'File uploaded successfully',
+        data: { publicUrl }
+    });
+});
+
+apiRout.delete(async (req: NextApiRequest, res: NextApiResponse) => {
+    const { fileId } = req.body;
+    const deletedFile = await deleteFile(fileId);
+    if (!deletedFile) {
+        return res.status(500).json({
+            message: 'Error deleting file'
+        });
+    }
+    return res.status(200).json({
+        message: 'File deleted successfully',
+        data: deletedFile
+    });
+})
+
+export default apiRout;
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+}
+
