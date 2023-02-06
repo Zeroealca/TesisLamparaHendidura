b6e70789af682d1480615f042c9a1c7ee745d0fe
Bryan Macias <bm577703@gmail.com>
2023-02-06 15:05:28 -0500

    No se que hace esto

--- src/components/simulator/simulatorButton.tsx
+++ src/components/simulator/simulatorButton.tsx
@@ -11,13 +11,15 @@ const SimulatorButton = ({ name, onClick, icon }: SimulatorButtonProps) => {
       onClick={onClick}
       className="flex items-center justify-center gap-5 py-2 px-10 bg-blacktertiary rounded-lg hover:bg-blacktertiary/60 cursor-pointer"
     >
-      <Icon
-        children={icon}
-        fill="#212121"
-        viewBox="16 16"
-        height={35}
-        width={35}
-      />
+      {icon && (
+        <Icon
+          children={icon}
+          fill="#212121"
+          viewBox="16 16"
+          height={35}
+          width={35}
+        />
+      )}
       <span className="font-bold text-lg">{name}</span>
     </button>
   );
